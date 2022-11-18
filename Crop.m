b835a1be30e22f193380303358465056a0f1488a
Yulius Agung <112539763+yuliusagungt@users.noreply.github.com>
2022-11-18 15:15:45 +0700

    Update Crop.m

--- Crop.m
+++ Crop.m
@@ -1,46 +1,37 @@
 pkg load image;
 clear;
 
-asli = imread("apel.jpg");
+asli = imread("apel2.jpg");
 biner = im2bw(asli);
-
 [baris, kolom] = size(biner);
+atasBawah = 0; a = 1;
+kiriKanan = 0; b = 1;
 
 for i = 1 : baris
   for j = 1 : kolom
     if biner(i,j) == 0
-      top = baris - i;
+      atasBawah(a) = i;
+      a++;
     end
   end
 end
-
-for i = 1 : baris
-  for j = 1 : kolom
-    if biner(i,j) == 0
-      bottom = i;
-    end
-  end
-end
+top = min(atasBawah);
+bottom = max(atasBawah);
 
 for i = 1 : kolom
   for j = 1 : baris
     if biner(j,i) == 0
-      left = kolom - i;
+      kiriKanan(b) = i;
+      b++;
     end
   end
 end
-
-for i = 1 : kolom
-  for j = 1 : baris
-    if biner(j,i) == 0
-      right = i;
-    end
-  end
-end
+left = min(kiriKanan);
+right = max(kiriKanan);
 
 crop = biner(top:bottom, left:right, :);
 
 figure(1);
-subplot(1,3,1); imshow(asli); title("Asli");
-subplot(1,3,2); imshow(biner); title("Biner");
-subplot(1,3,3); imshow(crop); title("Crop");
+subplot(2,2,1); imshow(asli); title("Asli");
+subplot(2,2,2); imshow(biner); title("Biner");
+subplot(2,2,3); imshow(crop); title("Crop");
